7a9413ef084f5309a873e630036d16d42cc15b91
TomSavas <tomas.savickas@protonmail.com>
2022-05-01 18:15:40 +0000

    Add zmath

--- build.zig
+++ build.zig
@@ -1,4 +1,5 @@
 const std = @import("std");
+const zmath = @import("libs/zmath/build.zig");
 
 pub fn build(b: *std.build.Builder) void {
     // Standard target options allows the person running `zig build` to choose
@@ -14,6 +15,9 @@ pub fn build(b: *std.build.Builder) void {
     const exe = b.addExecutable("rayzigger", "src/main.zig");
     exe.setTarget(target);
     exe.setBuildMode(mode);
+
+    exe.addPackage(zmath.pkg);
+
     exe.install();
 
     const run_cmd = exe.run();
